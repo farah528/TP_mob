c9eccbc82fa2c50eadac44fa995490ebffbc64ed
farah528 <farahotarid2@gmail.com>
2025-12-17 15:25:29 +0100

    first commit

--- TP/screens/TodoListScreen.js
+++ TP/screens/TodoListScreen.js
@@ -1,34 +1,7 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { addTodo } from "../store/todosSlice";
-import AppBar from "../components/AppBar";
-export default function TodoListScreen({ navigation }) {
-const todos = useSelector(state => state.todos);
-const dispatch = useDispatch();
+import { useTodoStore } from "../store/useTodoStore";
+const { todos, addTodo } = useTodoStore();
 useEffect(() => {
- dispatch(addTodo({ id: 1, title: "Faire les courses" }));
- dispatch(addTodo({ id: 2, title: "Sortir le chien" }));
- dispatch(addTodo({ id: 3, title: "Coder une app RN" }));
+ addTodo({ id: 1, title: "Faire les courses" });
+ addTodo({ id: 2, title: "Sortir le chien" });
+ addTodo({ id: 3, title: "Coder une app RN" });
 }, []);
-return (
- <View style={{ flex: 1, padding: 20 }}>
- <AppBar title="Mes tâches" />
- <FlatList
- data={todos}
- keyExtractor={(i) => i.id.toString()}
- renderItem={({ item }) => (
- <TouchableOpacity
- onPress={() =>
- navigation.navigate("Détails", item)
- }
- >
- <Text style={{ padding: 10, fontSize: 18 }}>
- {item.title}
- </Text>
- </TouchableOpacity>
- )}
- />
- </View>
-);
-}
